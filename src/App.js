ceef626cdcbc9fc3e4cec5036651ac4824abc261
dapawn <dapawn@gmail.com>
2021-12-07 11:22:59 -0500

    Added Navbar section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header>
-      </header>
+    <header>
+    <nav className="nav-wrapper transparent">
+      <div className="container">
+        <a href="#" className="brand-logo">Mosaic NE Philly Homeschool Coop</a>
+        <a href="#" className="sidenav-trigger" data-target="mobile-menu">
+          <i className="material-icons">menu</i>
+        </a>
+        <ul className="right hide-on-med-and-down">
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#what">What We Do</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <ul className="sidenav grey lighten-2" id="mobile-menu">
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#what">What We Do</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+    </header>
+
+
+
+
     </div>
   );
 }
