aec10cf92fa43331c90aeaa2979a70e0a5663e72
dapawn <dapawn@gmail.com>
2021-12-06 21:55:10 -0500

    Initialize project with create-react-app for Converting mosaic_site2 repository to a React app.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 import './index.css';
-
-import reduxThunk from 'redux-thunk';
-import * as serviceWorker from './serviceWorker';
-
-import App from './components/app';
-import rootReducer from './reducers';
-
-const store = createStore(rootReducer, applyMiddleware(reduxThunk));
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-    , document.getElementById('root'));
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
